52b41ef7ca6f6fa84c36cb1991902093433c147f
clodioneto <clodio.alves@gmail.com>
2021-05-06 23:18:51 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 import Datatable from "components/Datatable";
+import BarChart from "components/BarCharts";
+import DonutChart from "components/DonutCharts";
 
 
 
@@ -9,7 +11,25 @@ function App() {
     <>
       <NavBar></NavBar>
       <div className="container">
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+
+        <div className="row px-3">
+          
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Conversão (%)</h5>
+            <BarChart></BarChart>
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de conversão (%)</h5>
+            <DonutChart />
+          </div>
+        </div>
+
+        <div className="row px-3">
+          <h2 className="text-primary">Todas as Vendas</h2>
+        </div>
         <Datatable></Datatable>
+
       </div>
       <Footer></Footer>
     </>
--- frontend/src/components/Datatable/index.tsx
+++ frontend/src/components/Datatable/index.tsx
@@ -14,6 +14,41 @@ const Datatable = () => {
                 <tbody>
                     <tr>
                         <td>22/04/2021</td>
+                        <td>Nani</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Babicho</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>1500017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Giovanna</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Belinha</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Pompom</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
                         <td>Barry Allen</td>
                         <td>34</td>
                         <td>25</td>
